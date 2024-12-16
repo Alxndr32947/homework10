76bce3b5f2ee8c5f2f6bbef2ae537f2786a0100c
Alxndr32947 <alexsh32947@gmail.com>
2024-12-16 23:47:04 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -9,8 +9,8 @@ def get_multiplied_digits(number):
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
-        # Если осталась одна цифра, возвращаем её, что предотвращает бесконечную рекурсию.
-        return first
+        # Если осталась одна цифра, возвращаем её, но заменяем 0 на 1.
+        return first if first != 0 else 1 
 
 
 # Примеры использования функции
@@ -18,4 +18,4 @@ result = get_multiplied_digits(40203)
 print(result)  # Вывод: 24
 
 result2 = get_multiplied_digits(402030)
-print(result2)  # Вывод: 0?
+print(result2)  # Вывод: 24
